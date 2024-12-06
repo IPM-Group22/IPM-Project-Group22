1a8e5b0c52aefc218652a8abd12df6aff49bbe06
Tiago <a49510@alunos.isel.pt>
2024-12-06 00:00:08 +0000

    Final Project

--- FCTRoomScheduler/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import {createRoot} from "react-dom/client";
-import React from "react";
-import {Router} from "./Router";
-
-const root = createRoot(document.getElementById('container'));
-root.render(<Router/>);
